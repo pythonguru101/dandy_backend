f88ab99799bcccaae3a75f44f1a00665c71dcdda
pythonguru <pythonguru101@gmail.com>
2022-06-16 12:16:18 -0700

    Use waitress as production server

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from api_hub import settings_api_hub
+from waitress import serve
+import sys
 
 app = settings_api_hub()
 
 if __name__ == '__main__':
-    app.run()
+    if len(sys.argv) > 1:
+        ip = sys.argv[1]
+    else:
+        ip = '127.0.0.1'
+    print(ip)
+    serve(app, host=ip, port=5000)
